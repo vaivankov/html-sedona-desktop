e1515fae5f399716a3c742ca5497e092d863d063
vaivankov <ivankov.vitalii.2017@yandex.ru>
2020-07-06 17:48:48 +0300

    Добавлена функция, устанавливающая дату заезда/выезда

---
+++ js/func__setTodaysDate.js
@@ -0,0 +1,14 @@
+'use strict';
+
+(function () {
+  window.dateNow = new Date().getTime() + 604800000;
+  window.dateFull = new Date(dateNow);
+  window.dateFull = dateFull.toISOString();
+  window.dateShort = dateFull.match(/\d{4}-\d{2}-\d{2}/);
+  window.dateEnd = dateNow + 518400000;
+  window.dateEndFull = new Date(dateEnd);
+  window.dateEndFull = dateEndFull.toISOString();
+  window.dateEndShort = dateEndFull.match(/\d{4}-\d{2}-\d{2}/);
+  document.querySelector('#start-date').value = dateShort;
+  document.querySelector('#end-date').value = dateEndShort;
+})()
