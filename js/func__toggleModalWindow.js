9dee6ecb47dcae59ad0912fbebbd3ddada83d5ea
vaivankov <ivankov.vitalii.2017@yandex.ru>
2020-07-06 15:38:13 +0300

    Добавлена анимация появления/исчезновения модального окна

--- js/func__toggleModalWindow.js
+++ js/func__toggleModalWindow.js
@@ -1,8 +1,21 @@
 'use strict';
 
 (function () {
-  window.toggleModalWindow = function () {
-    modalHotels.classList.toggle("hide")
+  window.toggleModalWindow = function (evt) {
+    if (modalHotels.style.display === "block") {
+      modalHotels.classList.add("modal-hide-animation");
+      setTimeout(() => {
+        modalHotels.style.display = "none";
+        modalHotels.classList.remove("modal-hide-animation");
+        modalHotels.classList.remove("modal-show-animation")
+      }, 300);
+    } else {
+      modalHotels.style.display = "block";
+      modalHotels.classList.add("modal-show-animation");
+      setTimeout(() => {
+        modalHotels.classList.remove("modal-hide-animation");
+        modalHotels.classList.remove("modal-show-animation")
+      }, 300);      
+    };
   }
-}
-)();
+})();
